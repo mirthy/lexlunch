b7fdb15ec670728a16bd45239d0e99d73bff4938
Jeff Kwan <mirthy@gmail.com>
2014-08-15 20:27:23 +0000

    Updated schema

--- models/order.js
+++ models/order.js
@@ -5,13 +5,14 @@ var orderSchema = new Mongoose.Schema({
 	provider: {type:String, required:true},
 	date: {type:Date, required:true, default:Date.now},
 	miscCharges: {type:Number, required:true, default:0},
-	closeTime: {type:Date, required:true, default:Date.now(30*60*1000)}
+	closeTime: {type:Date, required:true, default:Date.now(30*60*1000)},
 	acquisitionMethod: {type:String, required:true},
-	status: {type:String, required:true}
+	status: {type:String, required:true},
+	orderItems: [{type:orderItem}]
 });
 
 
-userSchema.plugin(require('passport-local-mongoose'));
+orderSchema.plugin(require('passport-local-mongoose'));
 
 //create the model and add it to the exports
 var order = exports.Order = Mongoose.model('Order', orderSchema, 'Orders');
--- models/orderItem.js
+++ models/orderItem.js
@@ -1,15 +1,15 @@
 var Mongoose = require('../database').Mongoose;
 
-var orderSchema = new Mongoose.Schema({
-	user: {type:Schema.Types.ObjectId, required:true}
+var orderItemSchema = new Mongoose.Schema({
+	user: {type:Schema.Types.ObjectId, required:true},
 	item: {type:String, required:true, default:"Chicken Parm"},
 	price: {type:Number, required:true, default:Date.now},
 	orderId: {type:Schema.Types.ObjectId, required:true}
 });
 
 
-userSchema.plugin(require('passport-local-mongoose'));
+orderItemSchema.plugin(require('passport-local-mongoose'));
 
 //create the model and add it to the exports
-var order = exports.Order = Mongoose.model('Order', orderSchema, 'Orders');
+var order = exports.Order = Mongoose.model('OrderItem', orderItemSchema, 'OrderItems');
 
